77d6ccb7a5db5dde4061176f3271517e042aeb4c
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-08-16 01:02:34 +0300

    add decorator to limit the access to the pages

---
+++ accounts/decorators.py
@@ -0,0 +1,21 @@
+from functools import wraps
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import redirect
+
+def user_type_required(allowed_types):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                if request.user.user_type in allowed_types:
+                    return view_func(request, *args, **kwargs)
+                else:
+                    raise PermissionDenied
+            return redirect('login')  # Redirect to login page if user is not authenticated
+        return _wrapped_view
+    return decorator
+
+# now we can use this decorator like this
+# @user_type_required(['freelancer'])
+# def my_view(request):
+#     return render(request, 'my_view.html')
