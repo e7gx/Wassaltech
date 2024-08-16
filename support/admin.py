80a9ae69b6fe7e69725bda0209a525381416c46e
e7gx <abalgabo@gmail.com>
2024-08-17 01:34:18 +0300

    add the admin site

--- support/admin.py
+++ support/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from support.models import Ticket
+from support.models import Comment, Ticket
 
 # Register your models here.
 
 admin.site.register(Ticket)
+admin.site.register(Comment)
