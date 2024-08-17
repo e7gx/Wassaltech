38dba2050a8299a5dece56958774a47c9226d68c
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-08-17 15:00:46 +0300

    integrate, and enhance the ui of the tickets

--- support/models.py
+++ support/models.py
@@ -4,21 +4,21 @@ from accounts.models import Account
 
 # Create your models here.
 class Ticket(models.Model):
-    
+
     TICKET_CATEGORYS = (
-        ('general', 'General'),
-        ('technical', 'Technical'),
-        ('billing', 'Billing'),
-        ('complaint', 'Complaint'),
-        ('suggestion', 'Suggestion'),
-        ("feature_request", "Feature Request"),
-        ('other', 'Other'),
+        ('general', 'عامة'),
+        ('technical', 'تقنية'),
+        ('billing', 'الدفع'),
+        ('complaint', 'شكوى'),
+        ('suggestion', 'اقتراح'),
+        ("feature_request", "طلب اضافة ميزة"),
+        ('other', 'اخرى'),
     )
     class StatusChoices(models.TextChoices):
         OPEN = 'open', 'Open'
         IN_PROGRESS = 'in_progress', 'In Progress'
         CLOSED = 'closed', 'Closed'
-        
+
     ticket_creator = models.ForeignKey(Account, on_delete=models.CASCADE)
     ticket_category = models.CharField(max_length=255,choices=TICKET_CATEGORYS , default='general')
     ticket_title = models.CharField(max_length=255)
@@ -26,10 +26,10 @@ class Ticket(models.Model):
     ticket_status = models.CharField(max_length=20, choices=StatusChoices.choices, default=StatusChoices.OPEN)
     ticket_created_at = models.DateTimeField(auto_now_add=True)
 
-    
+
     def __str__(self):
         return f"{self.ticket_title} - {self.ticket_creator}"
-    
+
     def creator_full_name(self):
         return f"{self.ticket_creator.first_name} {self.ticket_creator.last_name}"
 
