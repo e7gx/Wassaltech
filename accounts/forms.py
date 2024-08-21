066d1d2494008d071fe584850050e83d18c5a06e
e7gx <abalgabo@gmail.com>
2024-08-21 20:42:45 +0300

    added the admin views and login from thr freelancerlogin

--- accounts/forms.py
+++ accounts/forms.py
@@ -49,23 +49,16 @@ class CustomerSignUpForm(forms.ModelForm):
 
 
 class FreelancerSignUpForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'أدخل كلمة المرور'}))
-    phone_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'أدخل رقم الهاتف'}))
-    address = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'أدخل العنوان'}))
-    certificate_id = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'أدخل رقم الشهادة'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter password'}))
+    phone_number = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Enter phone number'}))
+    address = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'placeholder': 'Enter address'}))
+    certificate_id = forms.CharField(max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Enter certificate ID'}))
     certificate_image = forms.ImageField(required=True, widget=forms.FileInput(attrs={'class': 'file-input file-input-bordered w-full max-w-xs'}))
     avatar = forms.ImageField(required=False, widget=forms.FileInput(attrs={'class': 'file-input file-input-bordered w-full max-w-xs'}))
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password', 'phone_number', 'address', 'certificate_id', 'certificate_image', 'avatar']
-        widgets = {
-            'username': forms.TextInput(attrs={'placeholder': 'أدخل اسم المستخدم'}),
-            'first_name': forms.TextInput(attrs={'placeholder': 'أدخل الاسم الأول'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'أدخل اسم العائلة'}),
-            'email': forms.EmailInput(attrs={'placeholder': 'أدخل البريد الإلكتروني'}),
-        }
-
 
     @transaction.atomic
     def save(self, commit=True):
@@ -73,14 +66,15 @@ class FreelancerSignUpForm(forms.ModelForm):
         user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
+            user_type = 'Freelancer'
+            if user.is_superuser:
+                user_type = 'Admin'
             Account.objects.create(
                 user=user,
                 phone_number=self.cleaned_data['phone_number'],
                 address=self.cleaned_data['address'],
-                user_type='Freelancer',
+                user_type=user_type,  # Set the user type here
                 avatar=self.cleaned_data['avatar']
-
-                
             )
             Freelancer.objects.create(
                 user=user,
