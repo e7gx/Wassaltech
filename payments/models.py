1e2f571e20f933c23d2c917aa19eb297f2363888
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-08-22 12:58:31 +0300

    Add Payment model to payments app
    Created a Payment model detailing its relationship with Offer and various status options. Also included fields for deposit due date, timestamps for creation and updates, and a string representation method.

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from orders.models import Offer
+
+
 # Create your models here.
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        ON_HOLD = 'OH', 'On Hold'
+        READY_TO_DEPOSIT = 'RTD', 'Ready to Deposit'
+        DEPOSITED = 'D', 'Deposited'
+
+    offer: models.OneToOneField(Offer, on_delete=models.PROTECT )
+    status: models.CharField(max_length=3,
+                             choices=Status.choices)
+    deposit_due_date: models.DateField()
+    updated_at: models.DateTimeField(auto_now=True)
+    created_at: models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
