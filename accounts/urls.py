68105a16a5a47632f6c57a374b0cd5fd9d772694
naif <naif.n1158@gmail.com>
2024-08-25 15:37:11 +0300

    done edit profile Customer

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('profile/customer/', views.customer_view_profile, name='customer_view_profile'),
     path('profile/freelancer/', views.freelancer_view_profile, name='profile'),
     path('profile/<int:freelancer_id>/', views.freelancer_profile, name='freelancer_profile'),
+    path('edit/profile/<int:user_id>/' , views.Edit_Profile , name='Edit_Profile'),
 ]
