fd3828e6f4aea0dca26b1e12a67d6a68a0b430b7
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-08-28 11:08:51 +0300

    add trusted origins

--- Wassaltech/settings.py
+++ Wassaltech/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = 'django-insecure-@ded6r$@8_x(a5dh=s9504&p9q4itwq35c((po^a22^ffpz^i8
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'wassaltech-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://wassaltech-production.up.railway.app']
+
 CORS_ALLOWED_ORIGINS = [
     "https://wassaltech-production.up.railway.app",
 ]
