5c15b85ef8e36073020c1a8f0342b92d511ef33c
e7gx <abalgabo@gmail.com>
2024-08-29 01:23:43 +0300

    update the ui and admin dashboard

--- analytics/views.py
+++ analytics/views.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from decimal import Decimal, ROUND_HALF_UP, ROUND_DOWN
-
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count, Sum, Avg, Q
+from django.db.models import Count, Sum, Avg, Q, F
 from django.contrib import messages
 
 from accounts.models import Account, Freelancer
@@ -17,6 +18,7 @@ from reviews.models import Review
 # Create your views here.
 @login_required
 def admin_dashboard(request):
+    payment = Payment.objects.all()
     Payment.process_payments()
     if request.user.is_superuser:
         orders_open = Order.objects.filter(status='Open').count()
@@ -61,6 +63,16 @@ def admin_dashboard(request):
         if most_category_tickets is not None:
             most_category_tickets = most_category_tickets['ticket_category']
 
+        
+        deposited_payments = Payment.objects.filter(status=Payment.Status.DEPOSITED).annotate(
+        payment_date_str=F('payment_date')
+        ).values('payment_date_str', 'amount').order_by('payment_date')
+
+        chart_data = list(deposited_payments)
+        serialized_data = json.dumps(chart_data, cls=DjangoJSONEncoder, default=str)
+        chart_data = serialized_data
+        print(chart_data)
+        
         context = {
             'orders_open': orders_open,
             'orders_in_progress': orders_in_progress,
@@ -73,7 +85,6 @@ def admin_dashboard(request):
             'total_customers': total_customers,
             'total_freelancers': total_freelancers,
             'total_admins': total_admins,
-
             'total_money_flow': total_money_flow,
             'wallet': wallet,
             'freelancer_wallet': freelancer_wallet,
@@ -85,6 +96,7 @@ def admin_dashboard(request):
             'tickets_open': tickets_open,
             'most_category_tickets': most_category_tickets,
             'user': request.user,
+            'chart_data': chart_data,
         }
         return render(request, 'analytics/admin_dashboard.html', context)
     else:
