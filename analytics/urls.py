41e33d992766c640f6d09ebffe3d2ea9fa39d9fd
e7gx <abalgabo@gmail.com>
2024-08-28 12:18:50 +0300

    update the admin payments and dashboard by Abdulrahman Alwagdani

--- analytics/urls.py
+++ analytics/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('customer/<int:pk>/', views.customer_profile, name='customer_profile'),
     path('freelancer-edit/<int:pk>/', views.edit_freelancer_profile, name='edit_freelancer_profile'),
     path('payment/', views.admin_payment, name='admin_payment'),
-    path('payment-deposit/', views.admin_deposit, name='admin_deposit'),
     path('admin_deposit_payment/<int:pk>/', views.admin_deposit_payment, name='admin_deposit_payment'),
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -27,28 +27,27 @@ def admin_dashboard(request):
         if best_catgorie is not None:
             best_catgorie = best_catgorie['category']
         rating = Review.objects.aggregate(Avg('rating'))['rating__avg']
-        orders_count = Offer.objects.all().count()
+        orders_count = Order.objects.all().count()
         total_users = Account.objects.all().count()
 
-        # wallet = Offer.objects.filter(stage="Accepted", order__status="Completed").all()
-        # total_wallet = wallet.aggregate(Sum('price'))['price__sum']#! we need to check the wallet of the Project Wassaltech
+     
         total_customers = Account.objects.all().filter(user_type='Customer').count()
         total_freelancers = Account.objects.all().filter(user_type='Freelancer').count()
         total_admins = Account.objects.all().filter(user_type='Admin').count()
 
-        # total_amount_pending_deposit = Payment.objects.filter(Q(status='Processing') | Q(status='Processed')).aggregate(
-        #     total_amount=Sum('amount'))['total_amount']
-        # total_refund_pending_deposit = Payment.objects.filter(Q(status='Processing') | Q(status='Processed')).aggregate(
-        #     total_refund_amount=Sum('refund_amount'))['total_refund_amount']
+       
         total_amount_deposited = Payment.objects.filter(Q(status='Deposited')).aggregate(total_amount=Sum('amount'))[
             'total_amount']
         total_refund_deposited = Payment.objects.filter(Q(status='Deposited')).aggregate(total_refund_amount=Sum('refund_amount'))[
             'total_refund_amount']
-        total_money_flow = total_amount_deposited + total_refund_deposited if total_amount_deposited and total_refund_deposited else 0
-        wallet = (total_amount_deposited * Decimal(0.1)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP) if total_amount_deposited else 0
-        freelancer_wallet = total_amount_deposited - wallet if total_amount_deposited else 0
-        customer_wallet = total_refund_deposited.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP) if total_refund_deposited else 0
-
+        
+        total_amount_deposited = Decimal(total_amount_deposited or 0)
+        total_refund_deposited = Decimal(total_refund_deposited or 0)
+        total_money_flow = total_amount_deposited + total_refund_deposited
+        wallet = (total_amount_deposited * Decimal(0.1)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        freelancer_wallet = total_amount_deposited - wallet
+        customer_wallet = total_refund_deposited.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        
         tickets_count = Ticket.objects.all().count()
         ticket_status_count = Ticket.objects.values('ticket_status').annotate(Count('ticket_status')).order_by('-ticket_status__count').first()
         if ticket_status_count is not None:
@@ -74,8 +73,7 @@ def admin_dashboard(request):
             'total_customers': total_customers,
             'total_freelancers': total_freelancers,
             'total_admins': total_admins,
-            # 'total_amount_deposited': total_amount_deposited,
-            # 'total_refund_deposited': total_refund_deposited,
+
             'total_money_flow': total_money_flow,
             'wallet': wallet,
             'freelancer_wallet': freelancer_wallet,
@@ -87,7 +85,6 @@ def admin_dashboard(request):
             'tickets_open': tickets_open,
             'most_category_tickets': most_category_tickets,
             'user': request.user,
-        #     'total_wallet': total_wallet,  # Why?
         }
         return render(request, 'analytics/admin_dashboard.html', context)
     else:
@@ -143,7 +140,7 @@ def customer_profile(request, pk):
         else:
             return redirect('main:index')
     
-    
+ 
 @login_required
 def edit_freelancer_profile(request: HttpRequest, pk: int) -> HttpResponse:
     if request.user.is_superuser:
@@ -191,13 +188,6 @@ def admin_payment(request):
         else:
             return redirect('main:index')
         
-@login_required
-def admin_deposit(request):
-    if request.user.is_superuser:
-        Payment.deposit_payments()
-        return redirect('analytics:admin_payment')
-    else:
-        return redirect('main:index')
     
     
 @login_required
