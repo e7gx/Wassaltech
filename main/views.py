2ab8f1d36b4d44fffeb7ec84c658b9e339883e77
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-08-29 07:45:39 +0300

    customize a sidebar to the admin

--- main/views.py
+++ main/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    if request.user.is_authenticated:
+        if request.user.account.user_type != 'Admin':
+            return render(request, 'main/index.html')
+        else:
+            return redirect('analytics:dashboard')
+    else:
+        return render(request, 'main/index.html')
 def about(request):
     return render(request , 'main/about.html')
 def terms_of_use(request):
